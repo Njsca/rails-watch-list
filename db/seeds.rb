3d9c3b09d67b64eb3d3f37697a867490fc040ccf
Denis Cantor <deniscantor7@gmail.com>
2022-08-18 13:04:39 +0200

    DB seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+# puts "Clearing DB..."
+# Movie.destory_all()
+
+puts "Seeding DB..."
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+result = JSON.parse(response)
+array = result["results"]
+
+# 10.times do
+#   i = 0
+#   movie = Movie.new(
+#     title: array[i]["title"],
+#     poster_url: array[i]["poster_path"],
+#     overview: array[i]["overview"],
+#     rating: array[i]["vote_average"]
+#   )
+#   i += 1
+#   movie.save!
+# end
+
+array.each_with_index do |hash, i|
+  movie = Movie.new(
+    title: hash["title"],
+    poster_url: hash["poster_path"],
+    overview: hash["overview"],
+    rating: hash["vote_average"]
+  )
+  movie.save!
+end
+
+puts "Done!"
