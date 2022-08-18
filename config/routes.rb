9750d25571758e3edace407e9654155d3f1aa865
Denis Cantor <deniscantor7@gmail.com>
2022-08-18 15:43:45 +0200

    features implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # lists routes
+  root 'lists#index'
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show', as: 'list'
+  # post 'lists', to: 'lists#create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # bookmarks routes
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,41 +3,44 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-require "json"
-require "open-uri"
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
 
-# puts "Clearing DB..."
-# Movie.destory_all()
+puts 'Clearing DB...'
+Movie.destroy_all
 
-puts "Seeding DB..."
+puts 'Seeding DB...'
 
-url = "https://tmdb.lewagon.com/movie/top_rated"
+url = 'https://tmdb.lewagon.com/movie/top_rated'
 response = URI.open(url).read
 result = JSON.parse(response)
-array = result["results"]
+array = result['results']
 
 # 10.times do
 #   i = 0
 #   movie = Movie.new(
-#     title: array[i]["title"],
-#     poster_url: array[i]["poster_path"],
-#     overview: array[i]["overview"],
-#     rating: array[i]["vote_average"]
+#     title: array[i]['title'],
+#     poster_url: array[i]['poster_path'],
+#     overview: array[i]['overview'],
+#     rating: array[i]['vote_average']
 #   )
 #   i += 1
 #   movie.save!
 # end
 
-array.each_with_index do |hash, i|
+array.each_with_index do |hash, _i|
   movie = Movie.new(
-    title: hash["title"],
-    poster_url: hash["poster_path"],
-    overview: hash["overview"],
-    rating: hash["vote_average"]
+    title: hash['title'],
+    poster_url: hash['poster_path'],
+    overview: hash['overview'],
+    rating: hash['vote_average']
   )
   movie.save!
 end
 
-puts "Done!"
+puts 'Done!'
+
+
+# `https://image.tmdb.org/t/p/w200#{hash['poster_path']}`,
